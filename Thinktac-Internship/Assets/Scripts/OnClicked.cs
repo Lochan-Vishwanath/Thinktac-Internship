c9f876c2aeb7c8ee5f9bcab49d486a273a9c98ed
Lochan-Vishwanath <33321391+Lochan-Vishwanath@users.noreply.github.com>
2018-09-10 22:11:24 +0530

    Game1 Portotype
    Chrome error ....Yet to create Full on clicked event.

---
+++ Thinktac-Internship/Assets/Scripts/OnClicked.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClicked : MonoBehaviour {
+
+	[SerializeField]
+	GameObject gobj;
+	void OnMouseDown(){
+		//Debug.Log("Clicked");
+		gobj.SetActive(true);
+	}
+
+}
