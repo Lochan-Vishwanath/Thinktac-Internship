47d05762dd77187485ed6d832bdfb61ae0565e5e
Lochan-Vishwanath <33321391+Lochan-Vishwanath@users.noreply.github.com>
2018-09-11 20:58:16 +0530

    Game1 Done.
    if clicked on wrong position implemented and next qustion-jump to next question not implemented.

---
+++ Thinktac-Internship/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public static byte NoOfAnswersFound=0;
+
+	public Text AnsFoundText;
+	public Button NextButton;
+
+	void Update(){
+
+		switch(NoOfAnswersFound){
+			case 1:AnsFoundText.text="You have found 1 Answer, Find the other 3.";break;
+			case 2:AnsFoundText.text="You have found 2 Answer, Find the other 2.";break;
+			case 3:AnsFoundText.text="You have found 3 Answer, Find the other 1.";break;
+			case 4:AnsFoundText.text="You have found all the answers, Goto next question.";break;
+		}
+		if(NoOfAnswersFound>=4){
+			NextButton.gameObject.SetActive(true);	
+		}
+		
+	}
+
+}
--- Thinktac-Internship/Assets/Scripts/OnClicked.cs
+++ Thinktac-Internship/Assets/Scripts/OnClicked.cs
@@ -1,14 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OnClicked : MonoBehaviour {
 
 	[SerializeField]
-	GameObject gobj;
-	void OnMouseDown(){
-		//Debug.Log("Clicked");
-		gobj.SetActive(true);
+	GameObject WrongMark;
+	[SerializeField]
+	GameObject MakerObj;
+	[SerializeField]
+	Image Rectobj;
+	AudioSource CorrectAudio;
+	float EntryTime,WrongTime;
+	bool OperationStart=false,MouseOnObject=false,EnterOnceOnly=true;
+	[SerializeField]
+	Text DisplayAnswerField,SideAnswerField;
+
+	void Start(){
+		CorrectAudio=GetComponent<AudioSource>();
 	}
+	void OnMouseDown(){
+		EntryTime=Time.time;
+		OperationStart=true;
+		CorrectAudio.Play();
+		if(EnterOnceOnly)
+			GameManagerScript.NoOfAnswersFound++;
+	}
+
+	void DoOpertaion(){
+			if(EntryTime+0.1f<=Time.time){
+				Rectobj.gameObject.SetActive(true);
+				if(EntryTime+0.2f<=Time.time){
+					DisplayAnswerField.gameObject.SetActive(true);
+					if(EntryTime+2.2f<=Time.time){
+						MakerObj.SetActive(true);
+						SideAnswerField.gameObject.SetActive(true);
+						Rectobj.gameObject.SetActive(false);
+						DisplayAnswerField.gameObject.SetActive(false);
+						OperationStart=false;
+						EnterOnceOnly=false;
+					}	
+				}
+			}
+	}
+
+	void Update(){
+		if(OperationStart){
+			DoOpertaion();
+		}
+	/*	if(Input.GetMouseButtonDown(0) && !MouseOnObject)
+  		{
+			WrongTime=Time.time;
+  			WrongMark.SetActive(true);
+   		}
+		if(WrongTime+0.3f<=Time.time){
+			WrongMark.SetActive(false);
+		}
+	*/
+	}
+
+	void OnMouseEnter(){
+ 		MouseOnObject = true;
+ 	}
+	 void OnMouseExit(){
+		MouseOnObject = false;
+	 }
 
 }
